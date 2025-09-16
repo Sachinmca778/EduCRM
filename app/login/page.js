6b9a4309ab96422d1df8dcd064b776c3e27d70e3
Kiran-Kumari88 <ky863255@gmail.com>
2025-09-16 11:47:33 +0530

    removed hello

--- app/login/page.js
+++ app/login/page.js
@@ -10,7 +10,6 @@ export default function LoginPage() {
     password: "",
     remember: false
   });
-  hello;
   const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async (e) => {
